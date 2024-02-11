729ca7feabc4c49dbd88189866a72341ec29b5d4
Jack <127067654+JackETobin@users.noreply.github.com>
2024-02-11 16:27:07 -0500

    Update and rename BiDET.h to Header/BiDET.h

--- Header/BiDET.h
+++ Header/BiDET.h
@@ -4,9 +4,9 @@
 #define KBtoBYTE 1024
 #define BD_NULL ((void*)0)
 
-typedef long int				int32_bd;
+typedef long int			int32_bd;
 typedef unsigned long int		uint32_bd;
-typedef long long				int64_bd;
+typedef long long			int64_bd;
 typedef unsigned long long		uint64_bd;
 
 #ifdef BiDET_ENABLE_DEBUG
@@ -29,10 +29,10 @@ typedef enum BiDET_Stash_Info	//RETURN TYPES:
 {
 	SPACE_TOTAL,				//uint64_bd
 	SPACE_REMAINING,			//uint64_bd
-	NUM_KEYS,					//uint64_bd
+	NUM_KEYS,				//uint64_bd
 	NUM_STASHES,				//uint64_bd
 	HANDLE_STASH,				//stash_bd*
-	HANDLE_KEYS					//key_bd*
+	HANDLE_KEYS				//key_bd*
 }bd_info;
 
 #ifdef BiDET_LARGE_BUFFER
@@ -69,32 +69,32 @@ void
 BiDET_Stash_Request(void* pInfoOut, uint32_bd stashID, bd_info infoReq);
 
 
-void														BiDET_Set_Callback(void(*pCallback)(bd_errpack));
-#define BD_SetErrCallback(pCallbackFunc)					BiDET_Set_Callback(pCallbackFunc)
+void									BiDET_Set_Callback(void(*pCallback)(bd_errpack));
+#define BD_SetErrCallback(pCallbackFunc)				BiDET_Set_Callback(pCallbackFunc)
 
-void														BiDET_Make_Stash(uint64_bd sizeBytes);
-#define BD_MakeStash(sizeKB)								BiDET_Make_Stash((uint64_bd)sizeKB * KBtoBYTE)
+void									BiDET_Make_Stash(uint64_bd sizeBytes);
+#define BD_MakeStash(sizeKB)						BiDET_Make_Stash((uint64_bd)sizeKB * KBtoBYTE)
 
-void														BiDET_Reset();
-#define BD_ClearStash										BiDET_Reset()
-
-
-#define BD_Store(keyName, data)								BiDET_Verify_Req(keyName, &data, &(uint64_bd){sizeof(data)}, STORE, BD_ERR_ID(keyName));
+void									BiDET_Reset();
+#define BD_ClearStash							BiDET_Reset()
 
 
-#define BD_Get(keyName, container)							BiDET_Verify_Req(keyName, &container, &(uint64_bd){sizeof(container)}, GET, BD_ERR_ID(keyName))
+#define BD_Store(keyName, data)						BiDET_Verify_Req(keyName, &data, &(uint64_bd){sizeof(data)}, STORE, BD_ERR_ID(keyName));
 
 
-#define BD_Remove(keyName)									BiDET_Verify_Req(keyName, BD_NULL, BD_NULL, REMOVE, BD_ERR_ID(keyName))
+#define BD_Get(keyName, container)					BiDET_Verify_Req(keyName, &container, &(uint64_bd){sizeof(container)}, GET, BD_ERR_ID(keyName))
 
 
-#define BD_Reserve(keyName, dataPtr, sizeBytes)						BiDET_Verify_Req(keyName, &dataPtr, &(uint64_bd){sizeBytes}, RESERVE, BD_ERR_ID(keyName))
+#define BD_Remove(keyName)						BiDET_Verify_Req(keyName, BD_NULL, BD_NULL, REMOVE, BD_ERR_ID(keyName))
+
+
+#define BD_Reserve(keyName, dataPtr, sizeBytes)				BiDET_Verify_Req(keyName, &dataPtr, &(uint64_bd){sizeBytes}, RESERVE, BD_ERR_ID(keyName))
 
 
 #define BD_Retrieve(keyName, dataPtr, dataSize)				BiDET_Verify_Req(keyName, &dataPtr, &dataSize, RETRIEVE, BD_ERR_ID(keyName))
 
 
-#define BD_Fill(keyName, data)								BiDET_Verify_Req(keyName, &data, &(uint64_bd){sizeof(data)}, FILL, BD_ERR_ID(keyName))
+#define BD_Fill(keyName, data)						BiDET_Verify_Req(keyName, &data, &(uint64_bd){sizeof(data)}, FILL, BD_ERR_ID(keyName))
 
 
 /*
@@ -103,7 +103,7 @@ void														BiDET_Reset();
 * INTO THE INFORET FIELD. INFORET RETURN TYPES ARE
 * DENOTED NEXT TO THE CORRESPONDING BD_INFO REQUEST.
 */
-void														BiDET_Stash_Request(void* pInfoOut, uint32_bd stashID, bd_info infoReq);
-#define BD_StashInfo(infoRet, stashID, requestedInfo)		BiDET_Stash_Request(&infoRet, stashID, requestedInfo)
+void									BiDET_Stash_Request(void* pInfoOut, uint32_bd stashID, bd_info infoReq);
+#define BD_StashInfo(infoRet, stashID, requestedInfo)			BiDET_Stash_Request(&infoRet, stashID, requestedInfo)
 
 #endif
