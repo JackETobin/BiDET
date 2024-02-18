0b09432836e83e26e37ceb0c54fea67695d5b0e8
Jack <127067654+JackETobin@users.noreply.github.com>
2024-02-18 11:31:33 -0500

    Update BiDET_Stash_Manager.c

--- Source/BiDET_Stash_Manager.c
+++ Source/BiDET_Stash_Manager.c
@@ -23,8 +23,7 @@ BiDET_Reset()
 	stash_bd* pStash = *(stash_bd**)BiDET_Retrieve_Stash();
 	pStash->numKeys = 0;
 
-	pStash->keyRing = pStash->nextStash = (void*)pStash + (pStash->sizeByte - voidPSize);
-	pStash->keyRing -= 1;
+	pStash->lastKey = pStash->nextStash = (void*)pStash + (pStash->sizeByte - voidPSize);
 	pStash->nextEntry = (void*)pStash + sizeof(stash_bd);
 
 	for (uint64_bd i = 0; (void*)pStash->nextEntry + i < (void*)pStash->nextStash; i++)
@@ -41,8 +40,7 @@ BiDET_Make_Stash(const uint64_bd sizeByte)
 	for (uint64_bd i = 0; i < sizeByte; i++)
 		*((char*)pStash + i) = 0;
 
-	pStash->keyRing = pStash->nextStash = (void*)pStash + (sizeByte - voidPSize);
-	pStash->keyRing -= 1;
+	pStash->lastKey = pStash->nextStash = (void*)pStash + (sizeByte - voidPSize);
 	pStash->nextEntry = (void*)pStash + sizeof(stash_bd);
 	pStash->sizeByte = sizeByte;
 
@@ -54,7 +52,7 @@ BiDET_Make_Stash(const uint64_bd sizeByte)
 
 
 static void
-Get_Requested_Info(void* pInfoOut, uint32_bd stashID, bd_info infoReq)
+Get_Requested_Info(void* pInfoOut, bd_info infoReq)
 {
 	stash_bd* activeStash = *BiDET_Retrieve_Stash();
 	switch (infoReq)
@@ -64,13 +62,19 @@ Get_Requested_Info(void* pInfoOut, uint32_bd stashID, bd_info infoReq)
 			break;
 
 		case SPACE_REMAINING:
-			void* lastEntry = activeStash->nextEntry;
-			void* lastKey = (void*)(activeStash->keyRing - (activeStash->numKeys - 1));
-			*(uint64_bd*)pInfoOut = lastKey - lastEntry;
+			*(uint64_bd*)pInfoOut = (void*)activeStash->lastKey - activeStash->nextEntry;
 			break;
 
 		case NUM_KEYS:
 			*(uint64_bd*)pInfoOut = activeStash->numKeys;
+			break;
+
+		case NUM_VOIDS:
+			uint64_bd numVoids = 0;
+			key_bd* pKey = activeStash->lastKey;
+			while(pKey->hash == BD_HASHMAX)
+			{ numVoids++; pKey++; }
+			*(uint64_bd*)pInfoOut = numVoids;
 			break;
 
 		case NUM_STASHES:
@@ -82,7 +86,7 @@ Get_Requested_Info(void* pInfoOut, uint32_bd stashID, bd_info infoReq)
 			break;
 
 		case HANDLE_KEYS:
-			*(key_bd**)pInfoOut = activeStash->keyRing - (activeStash->numKeys - (activeStash->numKeys > 0));
+			*(key_bd**)pInfoOut = activeStash->lastKey;
 			break;
 
 		default:
@@ -92,9 +96,9 @@ Get_Requested_Info(void* pInfoOut, uint32_bd stashID, bd_info infoReq)
 
 
 void
-BiDET_Stash_Request(void* pInfoOut, uint32_bd stashID, bd_info infoReq)
+BiDET_Stash_Request(void* pInfoOut, bd_info infoReq)
 {
-	uint32_bd requestVerification = ((pInfoOut != BD_NULL) && (stashID > 0));
-	(requestVerification) ? Get_Requested_Info(pInfoOut, stashID, infoReq) : 0;
+	uint32_bd requestVerification = (pInfoOut != BD_NULL);
+	(requestVerification) ? Get_Requested_Info(pInfoOut, infoReq) : 0;
 	return;
 }
